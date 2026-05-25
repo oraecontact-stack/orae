29f391872bb10335ee903122521bbf923318b51c
oraecontact-stack <orae.contact@gmail.com>
2026-05-25 08:14:19 +0000

    Update vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { fileURLToPath, URL } from "url";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import environment from "vite-plugin-environment";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+})
 
 const ii_url =
   process.env.DFX_NETWORK === "local"
